9d0da57079b25c09e471de958407939f20eb650e
Dan Funk <daniel.h.funk@gmail.com>
2018-05-16 11:40:58 -0400

    Better display of who can view a resource.

--- frontend/src/app/availability/availability.component.ts
+++ frontend/src/app/availability/availability.component.ts
@@ -4,7 +4,7 @@ import {Resource} from '../resource';
 @Component({
   selector: 'app-availability',
   templateUrl: './availability.component.html',
-  styleUrls: ['./availability.component.css']
+  styleUrls: ['./availability.component.scss']
 })
 export class AvailabilityComponent implements OnInit {
 
