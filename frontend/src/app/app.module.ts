edc38441e2f669e9d311e1f8a4db471e49fb3833
Aaron Louie <ajlouie@gmail.com>
2018-06-15 12:58:34 -0400

    Alphabetizes imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,32 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import { ResourceSearchComponent } from './resource-search/resource-search.component';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {AuthInterceptor} from './AuthInterceptor';
-import { ResourceComponent } from './resource/resource.component';
-import { ResourceListComponent } from './resource-list/resource-list.component';
-import {RouterModule, Routes} from '@angular/router';
-import {SearchComponent} from './search/search.component';
-import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import { AvailabilityComponent } from './availability/availability.component';
+import { BrowseComponent } from './browse/browse.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CategoryComponent } from './category/category.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 import {
   MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, MatCardModule,
   MatInputModule, MatCheckboxModule, MatFormFieldModule, MatTabsModule, MatProgressSpinnerModule,
   MatExpansionModule, MatSelectModule, MatListModule, MatRadioModule, MatStepperModule, MatSidenavModule,
   MatDialogModule, MatDatepickerModule, MatNativeDateModule, MatSnackBar, MatSnackBarModule
 } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {ReactiveFormsModule} from '@angular/forms';
-import {ResourceApiService} from './resource-api.service';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { AvailabilityComponent } from './availability/availability.component';
-import { BrowseComponent } from './browse/browse.component';
-import { CategoryComponent } from './category/category.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ResourceApiService } from './resource-api.service';
+import { ResourceComponent } from './resource/resource.component';
 import { ResourceFormComponent } from './resource-form/resource-form.component';
+import { ResourceListComponent } from './resource-list/resource-list.component';
+import { ResourceSearchComponent } from './resource-search/resource-search.component';
+import {AuthInterceptor} from './AuthInterceptor';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import {RouterModule, Routes} from '@angular/router';
+import {SearchComponent} from './search/search.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'search', pathMatch: 'full'},
@@ -36,8 +35,7 @@ const routes: Routes = [
   {path: 'browse/:category', component: BrowseComponent, data: {title: 'Browse'}},
   {path: 'category/:category', component: CategoryComponent, data: {title: 'Category'}},
   {path: 'category_form/:category', component: CategoryFormComponent, data: {title: 'Category Form'}},
-
-  ];
+];
 
 @NgModule({
   declarations: [
@@ -53,31 +51,35 @@ const routes: Routes = [
     ResourceFormComponent
   ],
   imports: [
-    CommonModule,
-    BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes, {useHash: true}),
-    HttpClientModule,
-    ReactiveFormsModule,
-    InfiniteScrollModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
+    BrowserModule,
+    CommonModule,
     FlexLayoutModule,
-    MatInputModule,
+    HttpClientModule,
+    InfiniteScrollModule,
+    MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
-    MatFormFieldModule,
-    MatTabsModule,
-    MatProgressSpinnerModule,
-    MatSelectModule,
-    MatExpansionModule,
-    MatListModule,
-    MatRadioModule,
-    MatStepperModule,
-    MatSidenavModule,
+    MatDatepickerModule,
     MatDialogModule,
-    MatDatepickerModule
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBar,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   entryComponents: [
     CategoryFormComponent, ResourceFormComponent
