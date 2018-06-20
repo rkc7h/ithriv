1fe52b0cbff91444a24e556a6a29a7df036453cb
Aaron Louie <ajlouie@gmail.com>
2018-06-20 12:34:40 -0400

    Moves category tile into separate component.

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,13 +1,12 @@
-
-import {debounceTime} from 'rxjs/operators';
-import {Component, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import {MatSidenav} from '@angular/material';
-
-import {Filter, ResourceQuery} from '../resource-query';
-import {Resource} from '../resource';
-import {ResourceApiService} from '../resource-api.service';
+import { Component, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSidenav } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+import { Category } from '../category';
+import { Resource } from '../resource';
+import { ResourceApiService } from '../resource-api.service';
+import { Filter, ResourceQuery } from '../resource-query';
 
 @Component({
   selector: 'app-search',
@@ -25,6 +24,7 @@ export class SearchComponent implements OnInit {
   searchBox: FormControl;
   loading = false;
   resources: Resource[];
+  categories: Category[];
 
   @ViewChild('sidenav') public sideNav: MatSidenav;
 
@@ -35,9 +35,17 @@ export class SearchComponent implements OnInit {
     private renderer: Renderer2
   ) {
     this.resources = [];
+    this.categories = [];
+
+    this.resourceService.getCategories().subscribe(
+      (categories) => {
+        this.categories = categories;
+      }
+    );
+
     this.route.params.subscribe(params => {
       const query = ('query' in params ? params['query'] : '');
-      this.resourceQuery = {query: query, filters: [], facets: [], total: 0, size: 20, start: 0, resources: []};
+      this.resourceQuery = { query: query, filters: [], facets: [], total: 0, size: 20, start: 0, resources: [] };
     });
     renderer.listen(window, 'resize', (event) => {
       this.checkWindowWidth();
@@ -66,12 +74,7 @@ export class SearchComponent implements OnInit {
       debounceTime(300)).subscribe(query => {
         this.resourceQuery.query = query;
         this.doSearch();
-    });
-  }
-
-  goBrowse($event, category) {
-    $event.preventDefault();
-    this.router.navigate(['browse', category]);
+      });
   }
 
   goSearch($event) {
@@ -119,7 +122,7 @@ export class SearchComponent implements OnInit {
   }
 
   addFilter(field: string, value: string) {
-    this.resourceQuery.filters.push({field: field, value: value});
+    this.resourceQuery.filters.push({ field: field, value: value });
     this.showFilters = false;
     this.resourceQuery.start = 0;
     this.doSearch();
