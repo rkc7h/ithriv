146eadd214da2b33caed9d445f655fe373c092f1
Aaron Louie <ajlouie@gmail.com>
2018-06-21 13:31:53 -0400

    Work in progress: Adds icons to Material Icon Registry. Adds icons to dropdown in category editing dialog

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,101 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public constructor(
+    public iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    const trustUrl = this.sanitizer.bypassSecurityTrustResourceUrl;
 
+    iconRegistry
+      .addSvgIcon(
+        'ithriv_admin_tools',
+        trustUrl('/assets/categories/admin_tools.svg')
+      )
+      .addSvgIcon(
+        'ithriv_ancillary_service',
+        trustUrl('/assets/categories/ancillary_service.svg')
+      )
+      .addSvgIcon(
+        'ithriv_center',
+        trustUrl('/assets/categories/center.svg')
+      )
+      .addSvgIcon(
+        'ithriv_certificate',
+        trustUrl('/assets/categories/certificate.svg')
+      )
+      .addSvgIcon(
+        'ithriv_clinical_translational',
+        trustUrl('/assets/categories/clinical_translational.svg')
+      )
+      .addSvgIcon(
+        'ithriv_communicate',
+        trustUrl('/assets/categories/communicate.svg')
+      )
+      .addSvgIcon(
+        'ithriv_community',
+        trustUrl('/assets/categories/community.svg')
+      )
+      .addSvgIcon(
+        'ithriv_data_management',
+        trustUrl('/assets/categories/data_management.svg')
+      )
+      .addSvgIcon(
+        'ithriv_data_science',
+        trustUrl('/assets/categories/data_science.svg')
+      )
+      .addSvgIcon(
+        'ithriv_design',
+        trustUrl('/assets/categories/design.svg')
+      )
+      .addSvgIcon(
+        'ithriv_facility',
+        trustUrl('/assets/categories/facility.svg')
+      )
+      .addSvgIcon(
+        'ithriv_funding',
+        trustUrl('/assets/categories/funding.svg')
+      )
+      .addSvgIcon(
+        'ithriv_grant',
+        trustUrl('/assets/categories/grant.svg')
+      )
+      .addSvgIcon(
+        'ithriv_grants',
+        trustUrl('/assets/categories/grants.svg')
+      )
+      .addSvgIcon(
+        'ithriv_logistics',
+        trustUrl('/assets/categories/logistics.svg')
+      )
+      .addSvgIcon(
+        'ithriv_management',
+        trustUrl('/assets/categories/management.svg')
+      )
+      .addSvgIcon(
+        'ithriv_participate',
+        trustUrl('/assets/categories/participate.svg')
+      )
+      .addSvgIcon(
+        'ithriv_regulations',
+        trustUrl('/assets/categories/regulations.svg')
+      )
+      .addSvgIcon(
+        'ithriv_research_reg',
+        trustUrl('/assets/categories/research_reg.svg')
+      )
+      .addSvgIcon(
+        'ithriv_special_topic',
+        trustUrl('/assets/categories/special_topic.svg')
+      )
+      .addSvgIcon(
+        'ithriv_training',
+        trustUrl('/assets/categories/training.svg')
+      )
+      .addSvgIcon(
+        'ithriv_writing',
+        trustUrl('/assets/categories/writing.svg')
+      );
+  }
 }
+
--- frontend/src/app/category-form/category-form.component.ts
+++ frontend/src/app/category-form/category-form.component.ts
@@ -1,13 +1,13 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {ResourceApiService} from '../resource-api.service';
-import {Category} from '../category';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ResourceApiService } from '../resource-api.service';
+import { Category } from '../category';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.css']
+  styleUrls: ['./category-form.component.scss']
 })
 export class CategoryFormComponent implements OnInit {
 
@@ -22,20 +22,26 @@ export class CategoryFormComponent implements OnInit {
   showConfirmDelete = false;
   error: string;
 
-  constructor(private api: ResourceApiService,
-              public dialogRef: MatDialogRef<CategoryFormComponent>,
-              @Inject(MAT_DIALOG_DATA) private data: any) {
-      if (this.data.edit) {
-        this.createNew = false;
-        this.category = this.data.edit;
-      } else {
-        this.createNew = true;
-        this.category = {id: null, name: '', description: ''};
-        if (this.data.parent_category) {
-          this.category.parent_id = this.data.parent_category.id;
-        }
+  constructor(
+    private api: ResourceApiService,
+    public dialogRef: MatDialogRef<CategoryFormComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any,
+  ) {
+    this.dialogRef.updatePosition({
+      top: '100px',
+    });
+
+    if (this.data.edit) {
+      this.createNew = false;
+      this.category = this.data.edit;
+    } else {
+      this.createNew = true;
+      this.category = { id: null, name: '', description: '' };
+      if (this.data.parent_category) {
+        this.category.parent_id = this.data.parent_category.id;
       }
-      this.loadForm();
+    }
+    this.loadForm();
   }
 
   ngOnInit() {
@@ -45,13 +51,12 @@ export class CategoryFormComponent implements OnInit {
     this.name = new FormControl([Validators.required, Validators.maxLength(256)]);
     this.description = new FormControl([Validators.required, Validators.minLength(20)]);
     this.brief_description = new FormControl([Validators.required, Validators.minLength(20),
-                                                        Validators.maxLength(200)]);
+    Validators.maxLength(200)]);
     this.icon = new FormControl();
 
     console.log('The category name ' + this.category.name);
 
-
-    this.categoryForm = new FormGroup( {
+    this.categoryForm = new FormGroup({
       name: this.name,
       description: this.description
     });
@@ -61,8 +66,8 @@ export class CategoryFormComponent implements OnInit {
     this.brief_description.patchValue(this.category.brief_description);
     this.icon.patchValue(this.category.icon);
 
-//    this.name.valueChanges.subscribe(t => this.category.name = t);
-//    this.description.valueChanges.subscribe(t => this.category.description = t);
+    //    this.name.valueChanges.subscribe(t => this.category.name = t);
+    //    this.description.valueChanges.subscribe(t => this.category.description = t);
 
     this.isDataLoaded = true;
   }
@@ -108,7 +113,42 @@ export class CategoryFormComponent implements OnInit {
       }
       this.dialogRef.close();
     },
-    error => this.error = error
+      error => this.error = error
     );
+  }
+
+  allIcons() {
+
+    // !!! TO DO
+    // Get list of icons from service URL
+    return [
+      'admin_tools',
+      'ancillary_service',
+      'center',
+      'certificate',
+      'clinical_translational',
+      'communicate',
+      'community',
+      'data_management',
+      'data_science',
+      'design',
+      'facility',
+      'funding',
+      'grant',
+      'grants',
+      'logistics',
+      'management',
+      'participate',
+      'regulations',
+      'research_reg',
+      'special_topic',
+      'training',
+      'writing'
+    ];
+  }
+
+  // Replaces underscores with spaces and capitalizes each word in given string
+  getLabel(s) {
+    return s.split('_').map(w => w[0].toUpperCase() + w.substr(1)).join(' ');
   }
 }
