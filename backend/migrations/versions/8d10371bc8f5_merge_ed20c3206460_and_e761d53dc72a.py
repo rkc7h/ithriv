dbb1651741256a1055d44aa60b03bd9161ec0ffb
Aaron Louie <ajlouie@ux-ninja.com>
2018-07-20 09:28:34 -0400

    Merges multiple migration heads

---
+++ backend/migrations/versions/8d10371bc8f5_merge_ed20c3206460_and_e761d53dc72a.py
@@ -0,0 +1,24 @@
+"""merge ed20c3206460 and e761d53dc72a
+
+Revision ID: 8d10371bc8f5
+Revises: ed20c3206460, e761d53dc72a
+Create Date: 2018-07-20 09:23:11.295283
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d10371bc8f5'
+down_revision = ('ed20c3206460', 'e761d53dc72a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
