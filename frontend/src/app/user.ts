db3b119fdcc2494d32a1eef975b9fb34b3d4eccc
Aaron Louie <ajlouie@ux-ninja.com>
2018-07-20 16:00:36 -0400

    Adds tabs to resource lists to switch between available & all resources

--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,6 +1,10 @@
+import { Links } from './links';
+
 export interface User {
   id: number;
   name: string;
   email_address: string;
   display_name: string;
+  institution_id: number;
+  _links: Links;
 }
