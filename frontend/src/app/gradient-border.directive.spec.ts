01fb84d6684a532833ecfbd7b936b25107d6c064
Aaron Louie <ajlouie@ux-ninja.com>
2018-08-07 13:09:24 -0400

    Adds gradient border directive

---
+++ frontend/src/app/gradient-border.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GradientBorderDirective } from './gradient-border.directive';
+
+describe('GradientBorderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GradientBorderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
