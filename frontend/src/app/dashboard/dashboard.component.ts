b4c1374b0637ef615a53289db93438d5a99c91e0
eleanor <eleanorcjkgraham@gmail.com>
2018-08-14 10:19:50 -0400

    changing dashboard .css to .scss

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import {ResourceApiService} from "../shared/resource-api/resource-api.service";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
