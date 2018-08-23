9b640b48fc4bb44c1bfac6bf8ac31bfec5b09faf
Dan Funk <daniel.h.funk@gmail.com>
2018-08-23 15:48:06 -0400

    Resolving merge.

---
+++ backend/migrations/versions/df7e4df7d800_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: df7e4df7d800
+Revises: d3e9fe2b05a2, e8f764d3a890
+Create Date: 2018-08-23 15:46:16.525882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'df7e4df7d800'
+down_revision = ('d3e9fe2b05a2', 'e8f764d3a890')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
