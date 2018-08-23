9eea59c5a56cb9d11db793c19dc2960315f518d5
eleanor <eleanorcjkgraham@gmail.com>
2018-08-23 15:46:31 -0400

    merging migrations and bringing the world back into harmony

---
+++ backend/migrations/versions/63c702552fed_merge_e8f764d3a890_and_d3e9fe2b05a2.py
@@ -0,0 +1,24 @@
+"""merge e8f764d3a890 and d3e9fe2b05a2
+
+Revision ID: 63c702552fed
+Revises: e8f764d3a890, d3e9fe2b05a2
+Create Date: 2018-08-23 15:44:49.377570
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63c702552fed'
+down_revision = ('e8f764d3a890', 'd3e9fe2b05a2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
