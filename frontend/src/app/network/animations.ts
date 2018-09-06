38a0a01b52a5aa87208528546b723993e83cf683
Aaron Louie <ajlouie@ux-ninja.com>
2018-09-06 14:33:14 -0400

    Moves object literals out of transition functions

--- frontend/src/app/network/animations.ts
+++ frontend/src/app/network/animations.ts
@@ -16,6 +16,35 @@ const easing = `${duration}ms ease-in-out`;
 const translate = 'translate( {{x}}px, {{y}}px )';
 const defaultParams = { params: { x: 0, y: 0 } };
 
+const shown = style({
+  transform: `${translate}`,
+  opacity: 1
+});
+
+const parked = style({
+  transform: 'translate(-200px, -200px) scale(0)',
+  opacity: 0
+});
+
+const lineHidden = style({ opacity: 0, transform: 'scale(0)' });
+const lineShown = style({ opacity: 1, transform: 'scale(1)' });
+const lineEasing = `${duration * 2}ms ease-in-out`;
+const hStates = ['void', 'parked', 'nary'];
+const vStates = ['root', 'child', 'primary', 'secondary', 'tertiary'];
+const v_to_h: string[] = [];
+const h_to_h: string[] = [];
+const h_to_v: string[] = [];
+const v_to_v: string[] = [];
+
+hStates.forEach(h1 => hStates.forEach(h2 => h_to_h.push(`${h1} => ${h2}`)));
+vStates.forEach(v1 => {
+  hStates.forEach(h => {
+    v_to_h.push(`${v1} => ${h}`);
+    h_to_v.push(`${h} => ${v1}`);
+  });
+  vStates.forEach(v2 => v_to_v.push(`${v1} => ${v2}`));
+});
+
 export function menuTransition(): AnimationTriggerMetadata {
   return trigger('menuState', [
     state('selected', style({
@@ -34,16 +63,6 @@ export function menuTransition(): AnimationTriggerMetadata {
 
 
 export function rootTransition(): AnimationTriggerMetadata {
-  const shown = style({
-    transform: `${translate}`,
-    opacity: 1
-  });
-
-  const parked = style({
-    transform: 'translate(-200px, -200px) scale(0)',
-    opacity: 0
-  });
-
   return trigger('rootState', [
     state('root', shown, defaultParams),
     state('child', shown, defaultParams),
@@ -111,55 +130,35 @@ export function grandchildPositionTransition(): AnimationTriggerMetadata {
 }
 
 export function lineTransition(): AnimationTriggerMetadata {
-  const hidden = style({ opacity: 0, transform: 'scale(0)' });
-  const shown = style({ opacity: 1, transform: 'scale(1)' });
-  const lineEasing = `${duration * 2}ms ease-in-out`;
-
-  const hStates = ['void', 'parked', 'nary'];
-  const vStates = ['root', 'child', 'primary', 'secondary', 'tertiary'];
-  const v_to_h: string[] = [];
-  const h_to_h: string[] = [];
-  const h_to_v: string[] = [];
-  const v_to_v: string[] = [];
-
-  hStates.forEach(h1 => hStates.forEach(h2 => h_to_h.push(`${h1} => ${h2}`)));
-  vStates.forEach(v1 => {
-    hStates.forEach(h => {
-      v_to_h.push(`${v1} => ${h}`);
-      h_to_v.push(`${h} => ${v1}`);
-    });
-    vStates.forEach(v2 => v_to_v.push(`${v1} => ${v2}`));
-  });
-
   return trigger('lineState', [
-    state('root', shown),
-    state('child', shown),
-    state('parked', hidden),
-    state('primary', shown),
-    state('secondary', shown),
-    state('tertiary', shown),
-    state('nary', hidden),
+    state('root', lineShown),
+    state('child', lineShown),
+    state('parked', lineHidden),
+    state('primary', lineShown),
+    state('secondary', lineShown),
+    state('tertiary', lineShown),
+    state('nary', lineHidden),
     transition(v_to_h.join(', '), [
       animate(lineEasing, keyframes([
-        shown,
-        hidden
+        lineShown,
+        lineHidden
       ]))
     ]),
     transition(h_to_h.join(', '), [
       animate(lineEasing, keyframes([
-        hidden
+        lineHidden
       ]))
     ]),
     transition(h_to_v.join(', '), [
       animate(lineEasing, keyframes([
-        hidden,
-        shown
+        lineHidden,
+        lineShown
       ]))
     ]),
     transition(v_to_v.join(', '), [
       animate(lineEasing, keyframes([
-        hidden,
-        shown
+        lineHidden,
+        lineShown
       ]))
     ])
   ]);
