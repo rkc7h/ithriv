4fbfa4620bcd5aab36000925ec795264b02916f4
eleanor <eleanorcjkgraham@gmail.com>
2018-09-06 13:18:22 -0400

    updating login service to get institution ids for the institutions without single signon (Carilion and Inova)

--- frontend/src/app/login-services/login-services.component.ts
+++ frontend/src/app/login-services/login-services.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { ResourceApiService } from "../shared/resource-api/resource-api.service";
 import { LoginService } from '../login-service';
 
 @Component({
@@ -11,29 +12,43 @@ export class LoginServicesComponent implements OnInit {
   loginServices: LoginService[] = [];
   loginUrl = environment.api + '/api/login';
 
-  constructor() {
+  constructor(
+    private api: ResourceApiService
+    ) {
     this.loadServices();
   }
 
   ngOnInit() {
   }
 
-  // !!! TO DO: Load available login services from the API
   loadServices() {
     const services = [
-      { id: 1, color: 'orange', name: 'UVA NetBadge', image: '/assets/institutions/UVA.png' },
+      { id: 1, color: 'orange', name: 'UVA NetBadge', image: '/assets/institutions/UVA.png', url: this.loginUrl },
       { id: 2, color: 'navy', name: 'Carilion', image: '/assets/institutions/Carilion.png' },
-      { id: 3, color: 'purple', name: 'Virginia Tech', image: '/assets/institutions/Virginia Tech.png' },
-      { id: 4, color: 'blue', name: 'Invoa', image: '/assets/institutions/Inova.png' },
+      { id: 3, color: 'purple', name: 'Virginia Tech', image: '/assets/institutions/Virginia Tech.png', url: this.loginUrl },
+      { id: 4, color: 'blue', name: 'Inova', image: '/assets/institutions/Inova.png' },
     ];
-
     this.loginServices = services.map(s => new LoginService(s));
   }
 
   goLoginService(loginService: LoginService) {
     console.log('loginService', loginService);
-    // !!! TO DO: Open web intent associated with given LoginService
-    window.location.href = this.loginUrl;
+    if (loginService.url) {
+      window.location.href = loginService.url;
+    } else {
+      this.api.getInstitutions().subscribe(institutions => {
+        for (const id in institutions) {
+          if (institutions.hasOwnProperty(id)) {
+            const inst = institutions[id];
+            if (inst.name == loginService.name) {
+              sessionStorage.setItem('institution_id', inst.id.toString());
+              sessionStorage.setItem('institution_name', loginService.name);
+              console.log('setting Name', loginService.name);
+              console.log('setting Id', inst.id.toString());
+            }
+          }
+        }
+      });
+    }
   }
-
 }
