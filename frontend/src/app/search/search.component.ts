13411b44514c6686c4e4aaf782886ac20c2dd663
eleanor <eleanorcjkgraham@gmail.com>
2018-09-18 09:57:47 -0400

    fixing the pagination index reset

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, Renderer2, ViewChild, HostBinding } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator, MatSidenav } from '@angular/material';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { debounceTime } from 'rxjs/operators';
 import { Category } from '../category';
 import { Resource } from '../resource';
@@ -29,13 +29,13 @@ export class SearchComponent implements OnInit {
   categories: Category[];
   publicId: number;
   user: User;
+  pageSize = 20;
 
   @ViewChild('sidenav') public sideNav: MatSidenav;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(
     private api: ResourceApiService,
-    private router: Router,
     private route: ActivatedRoute,
     private renderer: Renderer2
   ) {
@@ -53,7 +53,7 @@ export class SearchComponent implements OnInit {
     this.route.params.subscribe(params => {
       const query = ('query' in params ? params['query'] : '');
       const filter = ('field' && 'value' in params ? [{ field: params['field'], value: params['value'] }] : [] );
-      this.resourceQuery = { query: query, filters: filter, facets: [], total: 0, size: 10, start: 0, resources: [], sort: '_score' };
+      this.resourceQuery = { query: query, filters: filter, facets: [], total: 0, size: this.pageSize, start: 0, resources: [], sort: '_score' };
     });
     this.renderer.listen(window, 'resize', (event) => {
       this.checkWindowWidth();
@@ -80,8 +80,7 @@ export class SearchComponent implements OnInit {
     this.searchBox.setValue(this.resourceQuery.query);
     this.searchBox.valueChanges.pipe(
       debounceTime(300)).subscribe(query => {
-        this.resourceQuery.query = query;
-        this.doSearch();
+        this.updateQuery(query);
       });
   }
 
@@ -94,6 +93,7 @@ export class SearchComponent implements OnInit {
   updateQuery(query) {
     this.resourceQuery.query = query;
     this.resourceQuery.start = 0;
+    this.paginator.firstPage();
     this.doSearch();
   }
 
@@ -128,6 +128,7 @@ export class SearchComponent implements OnInit {
     this.resourceQuery.filters.push({ field: field, value: value });
     this.showFilters = false;
     this.resourceQuery.start = 0;
+    this.paginator.firstPage();
     this.doSearch();
   }
 
