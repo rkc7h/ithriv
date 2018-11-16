b82c2ed886cf4660a25f4295b18be07af326b248
Aaron Louie <ajlouie@ux-ninja.com>
2018-11-16 14:50:24 -0500

    Adds mocks for Markdown and Renderer2. Fixes a couple more unit tests.

--- frontend/src/app/category-color-border.directive.spec.ts
+++ frontend/src/app/category-color-border.directive.spec.ts
@@ -1,5 +1,45 @@
+import {
+  ElementRef,
+  Renderer2,
+  Component,
+  Type
+} from '@angular/core';
 import { CategoryColorBorderDirective } from './category-color-border.directive';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 
-describe('CategoryColorBorderDirective', () => {
-  it('should create an instance');
+class MockElementRef extends ElementRef { nativeElement = {}; constructor() { super(null); } }
+
+@Component({
+  template: `<div [appCategoryColorBorder]="resource"></div>`
+})
+class TestCategoryColorComponent { }
+
+fdescribe('CategoryColorBorderDirective', () => {
+  const el = new MockElementRef();
+  let renderer2: Renderer2;
+  let fixture: ComponentFixture<TestCategoryColorComponent>;
+
+  beforeEach(async(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [
+          TestCategoryColorComponent,
+          CategoryColorBorderDirective
+        ],
+        providers: [
+          { provide: ElementRef, useClass: MockElementRef },
+          Renderer2
+        ]
+      })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(TestCategoryColorComponent);
+        renderer2 = fixture.componentRef.injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
+      });
+  }));
+
+  it('should create an instance', () => {
+    const directive = new CategoryColorBorderDirective(el, renderer2);
+    expect(directive).toBeTruthy();
+  });
 });
--- frontend/src/app/category/category.component.spec.ts
+++ frontend/src/app/category/category.component.spec.ts
@@ -1,26 +1,58 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CategoryComponent } from './category.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTooltipModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MarkdownModule, MarkdownService } from 'ngx-markdown';
+import { Category } from '../category';
+import { getDummyCategory } from '../shared/fixtures/category';
+import { MockMarkdownService } from '../shared/mocks/markdown.service.mock';
+import { MockResourceApiService } from '../shared/mocks/resource-api.service.mock';
+import { ResourceApiService } from '../shared/resource-api/resource-api.service';
+import { CategoryComponent } from './category.component';
+import { getDummyResource } from '../shared/fixtures/resource';
+import { CategoryResource } from '../category-resource';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent;
   let fixture: ComponentFixture<CategoryComponent>;
+  let api: MockResourceApiService;
+  const category: Category = getDummyCategory();
+  const categoryResources: CategoryResource[] = [{
+    id: 0,
+    category_id: category.id,
+    resource: getDummyResource(),
+    category: category,
+    _links: { collection: '', self: '' }
+  }];
 
   beforeEach(async(() => {
+    api = new MockResourceApiService();
+
     TestBed
       .configureTestingModule({
         declarations: [CategoryComponent],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        imports: [
+          BrowserAnimationsModule,
+          MarkdownModule,
+          MatTooltipModule,
+          RouterTestingModule.withRoutes([])
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          { provide: ResourceApiService, useValue: api },
+          { provide: MarkdownService, useClass: MockMarkdownService },
+        ]
       })
-      .compileComponents();
+      .compileComponents()
+      .then(() => {
+        api.setResponse(category);
+        api.getCategorySpy(() => fixture.detectChanges());
+        fixture = TestBed.createComponent(CategoryComponent);
+        component = fixture.componentInstance;
+        component.categoryResources = categoryResources;
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CategoryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
