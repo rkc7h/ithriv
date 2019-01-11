f0b2b4ec7066d0ffa77d171489169d6035926cbd
Aaron Louie <ajlouie@ux-ninja.com>
2019-01-11 17:41:44 -0500

    Adds session status endpoint. Prints out all available endpoints.

---
+++ backend/app/resources/SessionStatusEndpoint.py
@@ -0,0 +1,26 @@
+import datetime
+import flask_restful
+from flask import g, jsonify
+import jwt
+from app import app
+
+
+class SessionStatusEndpoint(flask_restful.Resource):
+    """
+    Returns the timecode (in seconds) when the current session expires,
+    or 0 if there is no current session.
+    """
+
+    def get(self, auth_token):
+        if not auth_token:
+            return 0
+        else:
+            try:
+                payload = jwt.decode(
+                    auth_token,
+                    app.config.get('SECRET_KEY'),
+                    algorithms='HS256')
+
+                return payload['exp']
+            except Exception as e:
+                return 0
